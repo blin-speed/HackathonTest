a367b9d0d0b702e6a63f37d8de904cba1a73a00e
Rashmi-Pathak <rashmipathak2006@gmail.com>
2025-10-17 23:26:21 +0530

    rashmi made a changee

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
     public static void main (String[] args) {
         System.out.println("| Enter Name | Type a message |");
         System.out.println("| Sumedh | No risk, full push");
+        System.out.println("|Rashmi|No risk,full push");
     }
 }
