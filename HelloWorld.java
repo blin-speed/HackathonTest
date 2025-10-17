794500ce2b465d90f7279095ae8b7befcad01636
divyash17 <divyashmahadik@gmail.com>
2025-10-17 23:29:21 +0530

    Divyash made change

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld{
     public static void main (String[] args) {
         System.out.println("| Enter Name | Type a message |");
         System.out.println("| Sumedh | No risk, full push");
+        System.out.println("| Divyash  | simply lovely |");
+        
     }
 }
