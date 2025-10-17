5cfb0319e2049acdd2fb316e14808811da1d6b72
blin-speed <sumedh.malusare@outlook.com>
2025-10-17 21:07:21 +0530

    This is our test file, modify it as per ur liking

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+    public static void main (String[] args) {
+        System.out.println("| Enter Name | Type a message |");
+        System.out.println("| Sumedh | No risk full push");
+    }
+}
