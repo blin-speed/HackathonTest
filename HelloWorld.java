7afcc8e37613b7729362d3dc7a4207e05996609a
blin-speed <sumedh.malusare@outlook.com>
2025-10-18 00:03:53 +0530

    Forza Ferrari

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
     public static void main (String[] args) {
         System.out.println("| Enter Name | Type a message |");
-        System.out.println("| Sumedh | No risk, full push, simply lovely");
+        System.out.println("| Sumedh | No risk, full push, simply lovely|");
         System.out.println("|Rashmi|No risk,full push");
         System.out.println("|Divyash|namimiiii|");
     }
